1f07c91c5c59b7fc8cbfb23fb70e1c047b527da5
Hadian Alkani <sapatekno@gmail.com>
2023-02-28 15:29:11 +0700

    Dialog - Add option for third button

--- lib/src/feature/app/presenter/widget/dialog.widget.dart
+++ lib/src/feature/app/presenter/widget/dialog.widget.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_local.dart';
 import 'package:surveyami/src/util/string.util.dart';
 
+/// * Dialog with option yes and no (add option for third button)
 showConfirmDialog({
   required BuildContext context,
+  bool canDismissable = true,
   String? title,
   required String description,
   required String confirmBtnTxt,
   required String cancelBtnTxt,
   required Function onConfirmClicked,
+  String? thirdBtnTxt,
+  Function? thirdConfirmClicked,
 }) {
   /// * Cancel Button Like Cancel or No
   Widget cancelButton = TextButton(
@@ -15,11 +20,21 @@ showConfirmDialog({
     onPressed: () => Navigator.of(context).pop(),
   );
 
-  /// ? Confirm Button Like Yes or Confirm
+  /// * Confirm Button Like Yes or Confirm
   Widget confirmButton = ElevatedButton(
+    style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => Theme.of(context).primaryColorDark)),
     child: Text(confirmBtnTxt.toTitleCase()),
     onPressed: () {
       onConfirmClicked.call();
+      Navigator.of(context).pop();
+    },
+  );
+
+  /// * Third Option Button
+  Widget thirdButton = ElevatedButton(
+    child: Text((thirdBtnTxt ?? AppLocalizations.of(context)!.error).toTitleCase()),
+    onPressed: () {
+      if (thirdConfirmClicked != null) thirdConfirmClicked.call();
       Navigator.of(context).pop();
     },
   );
@@ -28,19 +43,25 @@ showConfirmDialog({
   AlertDialog alert = AlertDialog(
     title: title == null ? null : Text(title.toTitleCase(), style: const TextStyle(fontWeight: FontWeight.bold)),
     content: Text(description.toCapitalize(), style: const TextStyle(height: 1.5)),
-    actions: [cancelButton, confirmButton],
+    actions: [
+      cancelButton,
+      if (thirdBtnTxt != null) thirdButton,
+      confirmButton,
+    ],
   );
 
   /// * Show Dialog
-  _showDialog(context: context, builder: (context) => alert);
+  _showDialog(context: context, barrierDismissible: canDismissable, builder: (context) => alert);
 }
 
+/// * Dialog with only ok button
 showInfoDialog({
   required BuildContext context,
   String? title,
   required String description,
   required String confirmBtnTxt,
   Function? onConfirmClicked,
+  bool canDismissable = true,
 }) {
   /// * Confirm Button Like Yes or Confirm
   Widget confirmButton = ElevatedButton(
@@ -59,11 +80,16 @@ showInfoDialog({
   );
 
   /// *  Show the dialog
-  _showDialog(context: context, builder: (context) => alert);
+  _showDialog(context: context, barrierDismissible: canDismissable, builder: (context) => alert);
 }
 
-_showDialog({required BuildContext context, required WidgetBuilder builder}) {
+_showDialog({
+  required BuildContext context,
+  required WidgetBuilder builder,
+  required bool barrierDismissible,
+}) {
   return showDialog(
+    barrierDismissible: barrierDismissible,
     context: context,
     builder: builder,
   );
